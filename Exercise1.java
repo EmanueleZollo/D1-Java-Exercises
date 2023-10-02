f4708de56cb6cbe783570b59783b2ce6c38e78de
Emanuele Zollo - NjS Studio <ez.studionjs@gmail.com>
2023-10-02 14:14:49 +0200

    product method ex1

---
+++ Exercise1.java
@@ -0,0 +1,25 @@
+/*Esercizio #1
+        Implementare i seguenti metodi:
+
+        Moltiplica: accetta due interi e ritorna il loro prodotto
+        Concatena: accetta una stringa e un intero e restituisce una stringa che concatena gli elementi
+        inserisciInArray: accetta un array di stringhe di cinque elementi ed una stringa e restituisca un array di sei elementi in cui la stringa passata sia al terzo posto e le stringhe precedentemente in quarta e quinta posizione siano rispettivamente in quinta e sesta.
+
+        Scrivere una main che invochi in sequenza i tre metodi realizzati*/
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+        System.out.println(product(5, 9));
+/*
+        i metodi si scrivono esternamente al main,
+        i valori internamente?
+*/
+    }
+
+    public static int product(int n1, int n2) {
+        return n1*n2;
+    }
+
+
+}

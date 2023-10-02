f1057b06d5566deec79af098e792bfbaedd0d07f
Emanuele Zollo - NjS Studio <ez.studionjs@gmail.com>
2023-10-02 16:15:22 +0200

    Exercise3 first method

---
+++ Exercise3.java
@@ -0,0 +1,22 @@
+/*
+Esercizio #3
+        Implementare i seguenti metodi
+
+
+        perimetroRettangolo che accetta la lunghezza decimale dei due lati di un rettangolo e ne calcola il perimetro
+        pariDispari che accetta un numero intero e restituisce 0 se il numero è pari e 1 se è dispari
+        perimetroTriangolo che accetta le lunghezze decimali dei lati di un triangolo e ne restituisca l'area (è possibile usare la formula di Erone)
+
+        Scrivere una main che utilizzi in sequenza i tre metodi chiedendo l'input dei dati all'utente
+*/
+
+public class Exercise3 {
+    public static void main(String[] args) {
+
+        System.out.println(perimetroRettangolo(5.4,7.9));
+    }
+
+    public static double perimetroRettangolo (double side1, double side2) {
+        return (side1*2)+(side2*2);
+    }
+}

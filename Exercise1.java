821bd42790ffa602faafc7142570b208bd076970
Emanuele Zollo - NjS Studio <ez.studionjs@gmail.com>
2023-10-02 14:19:50 +0200

    concat string + int ex1 part 2

--- Exercise1.java
+++ Exercise1.java
@@ -3,7 +3,9 @@
 
         Moltiplica: accetta due interi e ritorna il loro prodotto
         Concatena: accetta una stringa e un intero e restituisce una stringa che concatena gli elementi
-        inserisciInArray: accetta un array di stringhe di cinque elementi ed una stringa e restituisca un array di sei elementi in cui la stringa passata sia al terzo posto e le stringhe precedentemente in quarta e quinta posizione siano rispettivamente in quinta e sesta.
+        inserisciInArray: accetta un array di stringhe di cinque elementi ed una stringa e restituisca
+         un array di sei elementi in cui la stringa passata sia al terzo posto e le stringhe precedentemente
+         in quarta e quinta posizione siano rispettivamente in quinta e sesta.
 
         Scrivere una main che invochi in sequenza i tre metodi realizzati*/
 
@@ -11,9 +13,9 @@ public class Exercise1 {
     public static void main(String[] args) {
 
         System.out.println(product(5, 9));
+        System.out.println(concatElements("Java", 1));
 /*
-        i metodi si scrivono esternamente al main,
-        i valori internamente?
+        i metodi si scrivono esternamente al main, i valori internamente?
 */
     }
 
@@ -21,5 +23,8 @@ public class Exercise1 {
         return n1*n2;
     }
 
+    public static String concatElements (String str,int n1) {
+        return str + n1;
+    }
 
 }
